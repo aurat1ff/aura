8e65a6fd112442a40f784affc3677278d8c4c0b2
aurat1ff <mprint637@gmail.com>
2025-07-23 12:09:41 -0700

    Add files via upload

--- functions.py
+++ functions.py
@@ -9,6 +9,7 @@ def map_role(role):
 
 def fetch_gemini_response(user_query):
     # Use the session's model to generate a response
-    response = st.session_state.chat_session.model.generate_content(user_query)
+    response = st.session_state.chat_session.model.generate_content(user_query,
+    timeout=300 # Increase timeout to 5 minutes (300 seconds))
     print(f"Aura's Response: {response}")
     return response.parts[0].text
